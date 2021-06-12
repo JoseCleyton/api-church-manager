d16b93f4c6a03ed8b0577c20801614621077ea75
José Cleyton <cleytoncomp@gmail.com>
2021-06-12 11:11:37 -0300

    develop - filtro por número do dizimista

--- src/main/java/com/church/manager/service/ChristianService.java
+++ src/main/java/com/church/manager/service/ChristianService.java
@@ -11,10 +11,10 @@ import com.church.manager.model.Christian;
 public interface ChristianService {
 	Christian save(Christian christian);
 	Page<Christian> findAll(Long idChurch, String name, int monthOfBirthday, Pageable pageable);
-	Page<Christian> findAll(Long iidChurchd, String name, Pageable pageable);
+	Page<Christian> findAll(Long idChurchd, String name, Pageable pageable);
 	List<Christian> retrieve(Long idChurch);
 	Long getQuantity(Long idChurch);
-	Optional<Christian> findById(Long id);
+	Optional<Christian> findById(Long id, Long idChurchd);
 	Christian update(Christian christian);
 	void delete(Long id);
 }
